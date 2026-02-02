58ca823169d1846c9ca220a43f18500c5a28f6da
mdourdoi <m.dourdoigne@hotmail.fr>
2026-02-02 22:26:02 +0100

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,22 @@
+def open_ancient_text() -> None:
+    """Open the selected file and read its content"""
+    try:
+        file_name = 'ancient_fragment.txt'
+        print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
+        print()
+        print(f"Accessing Storage Vault: {file_name}")
+        file = open('ancient_fragment.txt')
+        print("Connection established...")
+        print()
+        for line in file:
+            print(line, end="")
+        file.close()
+        print()
+        print()
+        print("Data recovery complete. Storage unit disconnected")
+    except FileNotFoundError:
+        print("The file 'ancient_fragment.txt' does not exists, aborting...")
+
+
+if __name__ == "__main__":
+    open_ancient_text()
